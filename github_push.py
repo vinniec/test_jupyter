e0de6f29693e1f15bf3022c7ef922a7496882721
vinniec <fz12345@gmail.com>
2017-05-21 06:07:41 +0200

    ho cambiato ancora lo script aggiuntando --all al commit perché non venivano più aggiornati dei file, speriamo bene ora

--- github_push.py
+++ github_push.py
@@ -1,5 +1,6 @@
 import subprocess
 import warnings
+from getpass import getpass
 
 def sh(cmdline):
     """run cmd in a subprocess and return its output.
@@ -19,21 +20,31 @@ status = sh('git status')
 for line in status.splitlines():
     if any([x in line for x in ['Changes', 'Untracked files:', '\t']]): print(line)
 
+
 #inserimento commit
 messag = input("inserisci messaggio di commit: ")
 try:
-    output = sh(f"""git commit -m '{messag}'""")
+    output = sh(f"""git commit --all -m '{messag}'""")
 except RuntimeError:
-    output = sh(f"""git commit --amend -m '{messag}'""")
+    output = sh(f"""git commit --all --amend -m '{messag}'""")
 print(output)
 
 #inserimento nome e password
+#nome = input("nome: ")
+#print('\033[1A{}'.format(" "*(6+len(nome))), '\033[1A')
+#pswd = input("pass: ")
+#print('\033[1A{}'.format(" "*(6+len(pswd))), '\033[1A')
+#print('ok')
 addr = sh('git config --get remote.origin.url') #oppure "git remote show origin"
-nome = input("nome: ")
-pswd = input("pass: ")
+nome = getpass("nome: ")
+pswd = getpass("pass: ")
 addr = addr.split('//')
 addr.insert(1, f"""//{nome}:{pswd}@""")
 addr = ''.join(addr) 
 
+##faccio un pull perché continua a rompere i coglioni, nun funziona
+#pull = sh('git pull')
+
+#costretto ad aggiungere il force per disperazione
 output = sh(f"""git push {addr}""")
 print(output)
